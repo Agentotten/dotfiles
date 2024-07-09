4fe75a105961695a267c11e272a780d42891982b
Kasper Agentoft Lind <kasperalind@gmail.com>
2024-07-09 12:15:41 +0200

    added hyprlock and hypridle

--- .config/nvim/lua/plugins/agentotten.lua
+++ .config/nvim/lua/plugins/agentotten.lua
@@ -1,18 +1,18 @@
 return {
-  -- override nvim-cmp and add cmp-emoji
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-    end,
-  },
+	-- override nvim-cmp and add cmp-emoji
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = { "hrsh7th/cmp-emoji" },
+		---@param opts cmp.ConfigSchema
+		opts = function(_, opts)
+			table.insert(opts.sources, { name = "emoji" })
+		end,
+	},
 
-  -- change some telescope options and a keymap to browse plugin files
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
+	-- change some telescope options and a keymap to browse plugin files
+	{
+		"nvim-telescope/telescope.nvim",
+		keys = {
       -- add a keymap to browse plugin files
       -- stylua: ignore
       {
@@ -20,46 +20,47 @@ return {
         function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
         desc = "Find Plugin File",
       },
-    },
-    -- change some options
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        clangd = {},
-        cssls = {},
-        jsonls = {},
-      },
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {
-        "bash",
-        "c",
-        "cpp",
-        "css",
-        "diff",
-        "glsl",
-        "json",
-        "lua",
-        "make",
-        "markdown",
-        "markdown_inline",
-        "xml",
-      },
-    },
-  },
+		},
+		-- change some options
+		opts = {
+			defaults = {
+				layout_strategy = "horizontal",
+				layout_config = { prompt_position = "top" },
+				sorting_strategy = "ascending",
+				winblend = 0,
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		---@class PluginLspOpts
+		opts = {
+			---@type lspconfig.options
+			servers = {
+				clangd = {},
+				cssls = {},
+				jsonls = {},
+			},
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"c",
+				"cpp",
+				"css",
+				"diff",
+				"glsl",
+				"hyprlang",
+				"json",
+				"lua",
+				"make",
+				"markdown",
+				"markdown_inline",
+				"xml",
+			},
+		},
+	},
 }
